405b946fa206fa7ce01f1731cc98b6af01582cbc
Anjana T S <anjanats2003@gmail.com>
2025-09-24 21:50:25 +0530

    day7_2 commit

---
+++ Day7_2.sql
@@ -0,0 +1,6 @@
+INSERT INTO mobile_app
+VALUES ('1','Raj','Chennai','88','5','Fitness'),('2','Anu','Mumbai','91',NULL,'Diet'),('3','Ravi','Chennai','78','3','Fitness'),('4','Meena','Delhi','82',NULL,'Diet'),('5','Farah','Mumbai','95','4','Fitness'),('6','Kiran','Pune','70',NULL,'Yoga'),('7','Latha','Pune','87',NULL,'Fitness');
+SELECT * FROM mobile_app
+WHERE score>(SELECT AVG(score) FROM mobile_app);
+SELECT name FROM mobile_app
+WHERE challenge=(SELECT challenge FROM mobile_app WHERE name='Farah');
